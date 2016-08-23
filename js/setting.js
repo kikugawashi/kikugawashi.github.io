aa8389340e412981c8731a34730fe5997b59de90
kikugawashi <hishokoho@city.kikugawa.shizuoka.jp>
2016-08-23 12:00:21 +0900

    不要CSV削除、SVG未使用
    不要CSV削除、SVG未使用

--- js/setting.js
+++ js/setting.js
@@ -1,9 +1,9 @@
-/*
+﻿/*
  * 5374 setting
  *
 */
 
-var SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
 var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
